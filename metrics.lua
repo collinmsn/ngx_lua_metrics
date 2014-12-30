a9c65a8be86fbac4e9cde0903da4f0516989e174
zhenghuabin <zhenghuabin@bytedance.com>
2014-12-31 00:11:53 +0800

    add emit timer and store

--- metrics.lua
+++ metrics.lua
@@ -1,23 +1,21 @@
 
 local metrics = {}
 local metrics_namespace_prefix = nil
-local host = nil
-local port = nil
+local udp = nil
 
 function metrics.init(conf)
     metrics_namespace_prefix = conf.metrics_namespace_prefix
-    host = conf.host
-    port = conf.port
+    local socket = require('socket')
+    udp = socket.udp()
+    udp:setpeername(conf.host, conf.port)
     ngx.log(ngx.NOTICE, "init metrics: prefix: ", metrics_namespace_prefix)
-    ngx.log(ngx.NOTICE, "init metrics: host: ", host)
-    ngx.log(ngx.NOTICE, "init metrics: port: ", port)
+    ngx.log(ngx.NOTICE, "init metrics: host: ", conf.host)
+    ngx.log(ngx.NOTICE, "init metrics: port: ", conf.port)
 end
 
 function metrics.send(req)
     local msgpack = require 'cmsgpack'
     msg = msgpack.pack(req)
-    local udp = ngx.socket.udp()
-    udp:setpeername(host, port)
     udp:send(msg)
 end
 
@@ -29,9 +27,35 @@ function metrics.c(name, prefix)
     end
 end
 
-function metrics.emit_counter(name, value, prefix)
+function metrics.tagkv2str(tagkv)
+    local tag_str = ''
+    for k, v in pairs(tagkv) do
+        if string.len(tag_str) > 0 then
+            tag_str = tag_str .. '|'
+        end
+        tag_str = tag_str .. string.format('%s=%s', k, v)
+    end
+    return tag_str
+end
+
+function metrics.emit_counter(name, value, prefix, tagkv)
     local cname = metrics.c(name, prefix)
-    req = {'emit', 'counter', cname, tostring(value), '', ''}
+    local tag_str = metrics.tagkv2str(tagkv)
+    req = {'emit', 'counter', cname, tostring(value), tag_str, ''}
+    metrics.send(req)
+end
+
+function metrics.emit_timer(name, value, prefix, tagkv)
+    local cname = metrics.c(name, prefix)
+    local tag_str = metrics.tagkv2str(tagkv)
+    req = {'emit', 'timer', cname, tostring(value), tag_str, ''}
+    metrics.send(req)
+end
+
+function metrics.emit_store(name, value, prefix, tagkv)
+    local cname = metrics.c(name, prefix)
+    local tag_str = metrics.tagkv2str(tagkv)
+    req = {'emit', 'store', cname, tostring(value), tag_str, ''}
     metrics.send(req)
 end
 
